f27044bfff2d6776ca8f32e25270789a9a938f39
Divyaraj <divyaraj.com>
2023-12-27 21:44:18 +0530

    initial

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -1,19 +1,31 @@
+import java.util.Arrays;
 public class MedianOfTwoSortedArrays{
-    public static int MedianOfTwoSortedArrayssol(int[] num1,int[] num2){
-        int median=0;
-        int total=(num1.length+num2.length);
-        int avg=(total/2);
-        int start=0,end=num1.length;
-        int pt1=(start+end)/2;
-        int pt2=avg-pt1;
+    public static float MedianOfTwoSortedArrayssol(int[] num1,int[] num2){
+        
+        int i=0,j=0,length=num1.length+num2.length,x=0;
+        float median=0;
+        int arr[]=new int[length];
 
-
+        while(i<num1.length){
+            arr[x++]=num1[i++];
+        }while(j<num2.length){
+            arr[x++]=num2[j++];
+        }
+        Arrays.sort(arr);
+        for(int k=0;k<arr.length;k++){
+            System.out.println(arr[k]);
+        }
+        if(length%2!=0){
+            median=arr[arr.length/2];
+        }else{
+          median= (((float)(arr[arr.length/2]))+((float)(arr[(arr.length/2)-1])))/2;
+        }
         return median;
     }
     public static void main(String[] args) {
         int num1[]={1,2,3};
-        int num2[]={4,5,6,7};
-        
+        int num2[]={4,5,6};
+
         System.out.println(MedianOfTwoSortedArrayssol(num1, num2));
     }
 }
